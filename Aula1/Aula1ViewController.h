7a49edea408bae58196ffcf299858b751ae422f1
Nome Teste <haenz07@gmail.com>
2011-10-04 22:39:00 -0300

    Commit com as alteracoes da aula1

--- Aula1/Aula1ViewController.h
+++ Aula1/Aula1ViewController.h
@@ -9,7 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface Aula1ViewController : UIViewController {
-    
+    //Criada uma label para que ela possa receber o texto futuramente
+    UILabel *labelTexto;
 }
+
+//Criada property para utilizarmos essa label como um Outlet, intregrando assim com o interface builder
+@property(nonatomic,retain) IBOutlet UILabel *labelTexto;
+
+
+//Criada ação para que quando o botão for pressionado ele acione o método que será escrito no Aula1ViewController.m
+-(IBAction)botaoPressionado:(id)sender;
 
 @end
--- Aula1/Aula1ViewController.m
+++ Aula1/Aula1ViewController.m
@@ -10,39 +10,49 @@
 
 @implementation Aula1ViewController
 
+//Sintetiza o Outlet labelTexto para ser utilizado aqui
+@synthesize labelTexto;
+
+//Implementação do metódo botaoPressionado, criado no Aula1ViewController.h
+-(IBAction)botaoPressionado:(id)sender{
+    
+    //O sender é recebido como parâmetro e possui a informação de qual botão foi pressionado
+    //Através dessa informação o ponteiro pTitulo apontará para o título escrito no botão que foi pressionado
+    NSString *pTitulo = [sender titleForState:UIControlStateNormal];
+    
+    //Pega o título escrito no botão que está sendo apontado pelo ponteiro pTitulo e escreve no texto, que terá seu endereço armazenado no ponteiro pTexto
+    NSString *pTexto = [[NSString alloc] initWithFormat:
+                       @"Botão %@ pressionado.", pTitulo];
+    
+    //O outlet labelTexto recebe o ponteiro pTexto, que aponta para o texto criado acima
+    labelTexto.text = pTexto;
+    
+    //Em seguida limpa-se a memória do ponteiro pTexto, pois não será mais usado o texto apontado por ele
+    [pTexto release];
+}
+
 - (void)dealloc
 {
+    //libera a memória que foi alocada para o Outlet labelTexto
+    [labelTexto release];
     [super dealloc];
 }
 
 - (void)didReceiveMemoryWarning
 {
-    // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
 }
 
-#pragma mark - View lifecycle
-
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-}
-*/
 
 - (void)viewDidUnload
 {
+    //O ponteiro Outlet recebe nulo para que ele não aponte mais para nenhum lugar, pois o endereço que ele apontava antes foi desalocado. Evitando assim possíveis erros de memória.
+    self.labelTexto = nil;
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
